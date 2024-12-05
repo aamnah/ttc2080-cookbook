c53ff9169beb6ee2c615e581ca624c15c4c5c20c
Aamnah <hello@aamnah.com>
2024-12-06 01:12:37 +0200

    + createCollection() & createRecipe()

--- frontend/api.ts
+++ frontend/api.ts
@@ -54,3 +54,76 @@ export async function fetchRecipesByTag(tag: string) {
     console.error(`API ERROR: Failed to get tag by name: ${tag} \n ${err}`);
   }
 }
+
+export async function createCollection(
+  collection: { title: string; thumbnail: string } = {
+    title: "",
+    thumbnail: "",
+  }
+) {
+  try {
+    const body = {
+      title: collection.title,
+      thumbnail: collection.thumbnail,
+    };
+    const response = await fetch(`${apiBaseUrl}/cookbooks/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      console.error(
+        `API ERROR: Response not okay. Failed to create collection: \n ${response}`
+      );
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(`API ERROR: Failed to create collection: ${err} \n ${err}`);
+  }
+}
+
+export async function createRecipe(recipe: {
+  title: string;
+  thumbnail?: string;
+  servings?: number;
+  prepTime?: string;
+  cookTime?: string;
+  totalTime?: string;
+  ingredients: string[];
+  directions: string[];
+}) {
+  try {
+    const body = {
+      title: recipe.title,
+      thumbnail: recipe.thumbnail ?? "/static/demo/placeholder.png",
+      servings: recipe.servings ?? 1,
+      prepTime: recipe.prepTime ?? "",
+      cookTime: recipe.cookTime ?? "",
+      totalTime: recipe.totalTime ?? "",
+      ingredients: recipe.ingredients ?? [],
+      directions: recipe.directions ?? [],
+    };
+
+    const response = await fetch(`${apiBaseUrl}/recipes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      console.error(
+        `API ERROR: Response not okay. Failed to create recipe: \n ${response}`
+      );
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(`API ERROR: Failed to create recipe: ${err} \n ${err}`);
+  }
+}
