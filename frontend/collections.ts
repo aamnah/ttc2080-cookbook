4cca0adaff84554691cebbad7bc9b6079fd08302
Aamnah <hello@aamnah.com>
2024-12-05 19:36:36 +0200

    Add New collection btn, use components

--- frontend/collections.ts
+++ frontend/collections.ts
@@ -1,25 +1,41 @@
 import renderSidebarHtml from "./components/sidebar";
 import { fetchCookbooks } from "./api";
 import renderCard from "./components/card";
+import renderHeader from "./components/header";
+import renderCardNew from "./components/cardNew";
 
 function renderCookbooks(cookbooks) {
   const container = document.querySelector("#contentContainer");
-  let html = ``;
-
-  cookbooks.map((item) => {
-    const { _id, title, image } = item;
-    html += renderCard({
-      id: _id,
-      title,
-      link: `recipe?id=${_id}`,
-    });
-  });
+  let html = `
+      <div>
+      <h2 class="text-4xl" data-editable>Collections</h2>
+      <div class="mt-2 flex flex-wrap gap-4">
+      ${renderCardNew({
+        title: "Add New Collection",
+        link: `collections/add`,
+        image: `/static/demo/placeholder_add.png`,
+      })}
+      ${cookbooks
+        .map((item) => {
+          const { _id, title, image } = item;
+          return renderCard({
+            id: _id,
+            title,
+            link: `collection?id=${_id}`,
+          });
+        })
+        .join("")}
+        </div>
+    </div>
+  `;
 
   container.innerHTML = html;
 }
 async function run() {
   const sidebarContainer = document.querySelector("#sidebarContainer");
+  const headerContainer = document.querySelector("#headerContainer");
   sidebarContainer.innerHTML = renderSidebarHtml();
+  headerContainer.innerHTML = renderHeader();
 
   const data = await fetchCookbooks();
 
