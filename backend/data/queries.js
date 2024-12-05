6f7cdbe79d66b0da705f826a642980ca1dcbe7a5
Aamnah <hello@aamnah.com>
2024-12-05 19:34:23 +0200

    test queries

--- backend/data/queries.js
+++ backend/data/queries.js
@@ -1,3 +1,23 @@
+db.cookbooks.updateMany(
+  { statuses: "inactive" }, // Find documents where "statuses" array contains "inactive"
+  { $set: { "statuses.$[]": "active" } } // Update all matching array elements
+);
+
+// WARNING!! Update ALL elements in the array
+db.cookbooks.updateMany(
+  { items: "Homemade Pizza" },
+  { $set: { "items.$[]": "6750d37b5237ce325c1c9a30" } }
+);
+
+// Update specific fields in array
+db.cookbooks.updateMany(
+  { "items.item": "Homemade Pizza" }, // Find documents where an element in "items" has "Homemade Pizza"
+  { $set: { "items.$[elem].item": "6750d37b5237ce325c1c9a30" } }, // Update specific matching element
+  {
+    arrayFilters: [{ "elem.item": "Homemade Pizza" }], // Apply filter to match specific array elements, make sure only items with "Homemade Pizza" are updated
+  }
+);
+
 // insertMany only works if the collection exists
 db.recipes.insertMany([
   {
