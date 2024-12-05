2daae0b007ccb2c1ac684933622a92c397551131
Aamnah <hello@aamnah.com>
2024-12-05 15:22:59 +0200

    getQueryParam()

--- frontend/helpers.ts
+++ frontend/helpers.ts
@@ -1,6 +1,20 @@
-export function getIdFromPath(path): string {
+export function getIdFromUrlPath(path): string {
   const parts = path.split("/");
   const id = parts[parts.length - 1];
   console.log(id);
   return id;
+}
+
+export function getQueryParam(location, param: string): string {
+  const queryString = location.search;
+  const urlParams = new URLSearchParams(queryString);
+  let value = "";
+
+  if (urlParams.has(param)) {
+    value = urlParams.get(param);
+    console.log(`${param}: ${value}`);
+    return value;
+  } else {
+    console.error(`ERROR: The param ${param} was not found`);
+  }
 }
