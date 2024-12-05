cb38824ff31be7659ad275a45bbb81fe8b885012
Aamnah <hello@aamnah.com>
2024-12-05 15:22:04 +0200

    fix endpoint URLS, remove constants.ts

--- frontend/api.ts
+++ frontend/api.ts
@@ -1,45 +1,45 @@
-import { apiEndpoint } from "./constants";
+const apiBaseUrl = "http://localhost:3000/api";
 
 export async function fetchRecipes() {
   try {
-    const response = await fetch(apiEndpoint.recipes);
+    const response = await fetch(`${apiBaseUrl}/recipes`);
     console.log(`response: ${response}`);
     const data = await response.json();
     console.log(`data: ${data}`);
 
     return data;
   } catch (err) {
-    console.error(`ERROR: Could not get recipes: ${err}`);
+    console.error(`API ERROR: Could not get recipes: ${err}`);
   }
 }
 
 export async function fetchRecipeById(id: string) {
   try {
-    const response = await fetch(`${apiEndpoint.recipes}/${id}`);
+    const response = await fetch(`${apiBaseUrl}/recipes/${id}`);
     const data = await response.json();
     return data;
   } catch (err) {
-    console.error(`ERROR: Could not get recipe by ID: ${id}\n ${err}`);
+    console.error(`API ERROR: Could not get recipe by ID: ${id}\n ${err}`);
   }
 }
 
 export async function fetchCookbooks() {
   try {
-    const response = await fetch(apiEndpoint.cookbooks);
+    const response = await fetch(`${apiBaseUrl}/cookbooks`);
     const data = await response.json();
     console.log(data);
     return data;
   } catch (err) {
-    console.error(`ERROR: Could not get cookbooks: ${err}`);
+    console.error(`API ERROR: Could not get cookbooks: ${err}`);
   }
 }
 
 export async function fetchCookbookById(id: string) {
   try {
-    const response = await fetch(`${apiEndpoint.cookbooks}/${id}`);
+    const response = await fetch(`${apiBaseUrl}/cookbooks/${id}`);
     const data = await response.json();
     return data;
   } catch (err) {
-    console.error(`ERROR: Could not get cookbook by ID: ${id} \n ${err}`);
+    console.error(`API ERROR: Could not get cookbook by ID: ${id} \n ${err}`);
   }
 }
