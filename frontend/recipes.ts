3ab96bc36e4c3b0d75f34fa929b2df69e9e0b44f
Aamnah <hello@aamnah.com>
2024-12-05 15:23:53 +0200

    + recipes listing page

--- frontend/recipes.ts
+++ frontend/recipes.ts
@@ -1,7 +1,30 @@
 import { fetchRecipes } from "./api";
+import renderSidebarHtml from "./components/sidebar";
+import renderCard from "./components/card";
+
+function renderRecipes(recipes) {
+  const container = document.querySelector("#contentContainer");
+  let html = ``;
+
+  recipes.map((item) => {
+    const { _id, title, image } = item;
+    html += renderCard({
+      id: _id,
+      link: `recipe?id=${_id}`,
+      title,
+    });
+  });
+
+  container.innerHTML = html;
+}
 
 async function run() {
+  const sidebarContainer = document.querySelector("#sidebarContainer");
+  sidebarContainer.innerHTML = renderSidebarHtml();
+
   const data = await fetchRecipes();
   console.log(`data: ${data}`);
+
+  renderRecipes(data);
 }
 run();
