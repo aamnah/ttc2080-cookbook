24d38c215412564b0980ce8af2e45f82887b5f6c
Aamnah <hello@aamnah.com>
2024-12-05 19:36:55 +0200

    Add New recipe btn, use components

--- frontend/recipes.ts
+++ frontend/recipes.ts
@@ -1,26 +1,42 @@
 import { fetchRecipes } from "./api";
 import renderSidebarHtml from "./components/sidebar";
-import renderCard from "./components/card";
+import renderCardHtml from "./components/card";
+import renderHeaderHtml from "./components/header";
+import renderCardNew from "./components/cardNew";
 
 function renderRecipes(recipes) {
   const container = document.querySelector("#contentContainer");
-  let html = ``;
-
-  recipes.map((item) => {
-    const { _id, title, image } = item;
-    html += renderCard({
-      id: _id,
-      link: `recipe?id=${_id}`,
-      title,
-    });
-  });
+  let html = `
+    <div>
+      <h2 class="text-4xl" data-editable>Recipes</h2>
+      <div class="mt-2 flex flex-wrap gap-4">
+      ${renderCardNew({
+        title: "Add New Recipe",
+        link: `recipes/add`,
+        image: `/static/demo/placeholder_add.png`,
+      })}
+      ${recipes
+        .map((item) => {
+          const { _id, title, image } = item;
+          return renderCardHtml({
+            id: _id,
+            link: `recipe?id=${_id}`,
+            title,
+          });
+        })
+        .join("")}
+      </div>
+    </div>
+    `;
 
   container.innerHTML = html;
 }
 
 async function run() {
   const sidebarContainer = document.querySelector("#sidebarContainer");
+  const headerContainer = document.querySelector("#headerContainer");
   sidebarContainer.innerHTML = renderSidebarHtml();
+  headerContainer.innerHTML = renderHeaderHtml();
 
   const data = await fetchRecipes();
   console.log(`data: ${data}`);
