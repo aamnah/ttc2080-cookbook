2f1acef5c0c462683b30e0ff7c307e88e5d9aec9
Aamnah <hello@aamnah.com>
2024-12-05 19:39:05 +0200

    + Tag page. list all recipes with a tag

---
+++ frontend/tag.ts
@@ -0,0 +1,43 @@
+import { fetchRecipesByTag } from "./api";
+import { getQueryParam } from "./helpers";
+import renderCardHtml from "./components/card";
+import renderSidebarHtml from "./components/sidebar";
+import renderHeader from "./components/header";
+
+function renderTag(tag, recipes) {
+  const container = document.querySelector("#contentContainer");
+  let html = `
+    <div>
+      <h2 class="text-4xl" data-editable>Tag: ${tag}</h2>
+      <div class="mt-2 flex flex-wrap gap-4">
+      ${recipes
+        .map((item) => {
+          const { _id, title, image } = item;
+          return renderCardHtml({
+            id: _id,
+            link: `recipe?id=${_id}`,
+            title,
+          });
+        })
+        .join("")}
+      </div>
+    </div>
+  `;
+
+  container.innerHTML = html;
+}
+async function run() {
+  const sidebarContainer = document.querySelector("#sidebarContainer");
+  sidebarContainer.innerHTML = renderSidebarHtml();
+  const headerContainer = document.querySelector("#headerContainer");
+  headerContainer.innerHTML = renderHeader();
+
+  const name = getQueryParam(location, "name");
+  const data = await fetchRecipesByTag(name);
+  console.log(`data: ${data}`);
+
+  document.title = `Tag: ${name}`;
+  renderTag(name, data);
+}
+
+run();
